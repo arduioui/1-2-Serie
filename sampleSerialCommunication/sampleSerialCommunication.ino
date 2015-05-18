6d626ea27d0c1ef0b59748a3739afce0060d0486
DAMIEN BRUN <makowildcat@gmail.com>
2015-05-18 16:09:27 +0200

    Add first sample

---
+++ sampleSerialCommunication/sampleSerialCommunication.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // main code here, to run repeatedly:
+  Serial.println("Hello");
+  delay(5000);
+}
